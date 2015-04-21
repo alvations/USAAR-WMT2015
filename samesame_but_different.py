8633eea685ac5b9001e9e042e283b3906c4d19fa
alvations <alvations@gmail.com>
2015-04-22 00:48:54 +0200

    added samsamebut different

---
+++ samesame_but_different.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import os, sys, time, itertools, io
+from collections import Counter
+
+from nltk.corpus import stopwords
+
+
+def sents(infile):
+    """ Lazy corpus reader that yields line. """
+    with io.open(infile, 'r', encoding='utf8') as fin:
+        for i, line in enumerate(fin):
+            yield line.strip()
+
+engstop = set(stopwords.words('english'))
+deustop = set(stopwords.words('german'))
+
+_engvocab = io.open('newstest.deen.en', 'r').read().split()
+_deuvocab = io.open('newstest.deen.de', 'r').read().split()
+
+engvocab = set(_engvocab).difference(engstop)
+deuvocab = set(_deuvocab).difference(deustop)
+
+testfile1 = '/media/2tb/wmt15/corpus.tok/train-de_en.en'
+testfile2 = '/media/2tb/wmt15/corpus.tok/train-de_en.en'
+
+fout = io.open('train-de_en', 'w', encoding='utf8')
+
+for engline, deuline in zip(sents(testfile1), sents(testfile2)):
+    engoverlap = set(engline.split()).difference(engvocab)
+    deuoverlap = set(deuline.split()).difference(deuvocab)
+    
+    if len(engoverlap) > 0 or len(deuoverlap) > 0:
+        outline = unicode(engline+'\t'+deuline+'\n')
+        print engoverlap
+        print deuoverlap
+        print outline
+        fout.write(outline)
+        
